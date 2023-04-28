cc8fc53280909d34fda3dc9ab30aa3b3a219b7c9
Chris Cromer <chris@cromer.cl>
2023-04-27 22:02:52 -0400

    finish challenge

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,20 @@ function updateRating(movieIndex, rating) {
 <template>
   <div class="app">
     <div class="movie-list">
-      <div
-        class="movie-item"
-        v-for="(movie, movieIndex) in movies"
-        :key="movie.id"
-      >
+      <div class="movie-item" v-for="(movie, movieIndex) in movies" :key="movie.id">
         <div class="movie-item-image-wrapper">
+          <div class="movie-item-image-star-wrapper">
+            <div class="movie-item-image-star-content-wrapper">
+              <div class="movie-item-image-star-icon-wrapper" :class="[
+                movie.rating ? 'text-yellow-500' : 'text-gray-500',
+              ]">
+                <StarIcon class="movie-item-image-star-icon" />
+                <div class="movie-item-image-text-icon">
+                  {{ movie.rating === 0 || movie.rating === null ? "-" : movie.rating }}
+                </div>
+              </div>
+            </div>
+          </div>
           <img :src="movie.image" class="movie-item-image" alt="" />
         </div>
 
@@ -24,12 +32,8 @@ function updateRating(movieIndex, rating) {
           <div class="movie-item-title-wrapper">
             <h3 class="movie-item-title">{{ movie.name }}</h3>
             <div class="movie-item-genres-wrapper">
-              <span
-                v-for="genre in movie.genres"
-                :key="`${movie.id}-${genre}`"
-                class="movie-item-genre-tag"
-                >{{ genre }}</span
-              >
+              <span v-for="genre in movie.genres" :key="`${movie.id}-${genre}`" class="movie-item-genre-tag">{{ genre
+              }}</span>
             </div>
           </div>
           <div class="movie-item-description-wrapper">
@@ -41,16 +45,9 @@ function updateRating(movieIndex, rating) {
             </span>
 
             <div class="movie-item-star-icon-wrapper">
-              <button
-                v-for="star in 5"
-                :key="star"
-                class="movie-item-star-icon-button"
-                :class="[
-                  star <= movie.rating ? 'text-yellow-500' : 'text-gray-500',
-                ]"
-                :disabled="star === movie.rating"
-                @click="updateRating(movieIndex, star)"
-              >
+              <button v-for="star in 5" :key="star" class="movie-item-star-icon-button" :class="[
+                movie.rating !== null && star <= movie.rating ? 'text-yellow-500' : 'text-gray-500',
+              ]" :disabled="star === movie.rating" @click="updateRating(movieIndex, star)">
                 <StarIcon class="movie-item-star-icon" />
               </button>
             </div>
